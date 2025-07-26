94aede79b528a53815e6d8659f08121b78755686
Shayan Bagheri <rekklescs@gmail.com>
2025-07-26 18:43:57 +0100

    feat: add Search model to log city lookups

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Search(models.Model):
+    city = models.CharField(max_length=100)
+    searched_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.city} at {self.searched_at:%Y-%m-%d %H:%M}"
