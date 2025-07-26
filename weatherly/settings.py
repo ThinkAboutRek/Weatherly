71b6ddebd6ec3891e6a33d0460da51e9c991f7bf
Shayan Bagheri <rekklescs@gmail.com>
2025-07-26 17:20:07 +0100

    feat: scaffold Django project and weather app with Postgres config

--- weatherly/settings.py
+++ weatherly/settings.py
@@ -1,22 +1,11 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x)@81o5is&k1%u5oje$=w#!ba$)@9f1c&@y3^rg9t432$7k40i'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -62,10 +51,14 @@ WSGI_APPLICATION = 'weatherly.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  "default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": os.getenv("POSTGRES_DB"),
+    "USER": os.getenv("POSTGRES_USER"),
+    "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+    "HOST": os.getenv("POSTGRES_HOST"),
+    "PORT": os.getenv("POSTGRES_PORT"),
+  }
 }
 
 
